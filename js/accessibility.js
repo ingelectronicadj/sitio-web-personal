bc7df749a3e02ca856a3c51bc2a9b05719d96bfb
Diego Mena <ingelectronicadj@gmail.com>
2024-02-25 21:03:02 -0500

    mejoro el modo de alto contraste incluyendo un tercer nivel de profundidad

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,4 +1,5 @@
-function contrast_type(classContrast) {
+/* Funcionalidad Modo Oscuro */
+function toggleContrast(classContrast) {
     //Para todos los textos
     $("h1,h2,h3,h4,h5,h6,p,a,em,span,strong,b,body").toggleClass(classContrast);
     //Para todos los bloques, contenedores del grid, barra de miga de pan, cabecera, menu 
@@ -9,33 +10,28 @@ function contrast_type(classContrast) {
     $(".fa-stack,.ir-arriba,.text-primary,.flags-lang,.prev,.next,.nav-link,.navbar-toggler-icon,#caption").toggleClass(classContrast);
 }
 
-var status_btn_contrast = document.getElementById("checkSwitch");
-var status_contrast_localStorage = localStorage.getItem("status_contrast");
-
-function set_contrast() {
-    var status_btn_contrast = document.getElementById("checkSwitch").checked;
+function setContrast() {
+    let status_btn_contrast = document.getElementById("checkSwitch").checked;
     localStorage.setItem("status_contrast", status_btn_contrast);
 }
 
-function get_contrast() {
-    var status_contrast_localStorage = localStorage.getItem("status_contrast");
+function getContrast() {
+    let status_contrast_localStorage = localStorage.getItem("status_contrast");
     if (status_contrast_localStorage == "true") {
         document.getElementById("checkSwitch").checked = true;
-        contrast_type("high-contrast");
+        toggleContrast("high-contrast");
     } else {
-        set_contrast();
+        setContrast();
     }
 }
 
-status_btn_contrast.addEventListener("click", set_contrast);
 
+/* Funcionalidad para dar foco a encabezados de sección 
+    de acuerdo a elementos de navegación */
 function handleClickFocus(event) {
     event.preventDefault(); // Evitar la acción predeterminada del enlace
-
     const targetId = this.getAttribute('href'); // Obtener el ID del destino
-
     const targetElement = document.querySelector(targetId); // Seleccionar el elemento destino
-
     if (targetElement) {
         targetElement.tabIndex = -1; // Establecer el índice de tabulación para permitir el enfoque
         targetElement.focus(); // Enfocar el elemento destino
@@ -44,7 +40,9 @@ function handleClickFocus(event) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    get_contrast();
+    const status_btn_contrast = document.getElementById("checkSwitch");
+    status_btn_contrast.addEventListener("click", setContrast);
+    getContrast();
 
     const menuLinks = document.querySelectorAll('.navbar-nav .nav-link');
     menuLinks.forEach(link => {
