4ec9e58e7d0d014856309ee5d523352b5bb121cf
Diego Mena <ingelectronicadj@gmail.com>
2024-02-25 19:10:25 -0500

    ajusto el modo contraste

--- js/accessibility.js
+++ js/accessibility.js
@@ -2,7 +2,7 @@ function contrast_type(classContrast) {
     //Para todos los textos
     $("h1,h2,h3,h4,h5,h6,p,a,em,span,strong,b,body").toggleClass(classContrast);
     //Para todos los bloques, contenedores del grid, barra de miga de pan, cabecera, menu 
-    $(".block,.panel,.gtopics,.breadcrumb,.container-fluid,header,.menu").toggleClass(classContrast);
+    $("div","section").toggleClass(classContrast);
     $("hr").toggleClass(classContrast);
     $("nav").toggleClass(classContrast);
     //Quita la seleccion de contraste
